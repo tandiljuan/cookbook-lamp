b0c60571a83554022924739c13b52af4be7c2c6b
Juan Manuel Lopez <tandiljuan@gmail.com>
2013-10-07 09:34:42 -0300

    BUGFIX: Apache isn't detected on first 'vagrant up'

--- recipes/php.rb
+++ recipes/php.rb
@@ -8,7 +8,13 @@ include_recipe "php"
 end
 
 # Install Apache's mod_php5 only if Apache is installed
-if system("type 'apache2'")
+# TODO: Remove the 'true' statement. It's a workaround to bypass the detection
+# of Apache. Apache is not being detected in the first 'vagrant up', but it is
+# detected in the second provision.
+# Alternatives to detect Apache could be:
+# * system('apache2 -v')
+# * File.directory?(node[:apache][:dir])
+if true && system("type 'apache2'")
   include_recipe "apache2::mod_php5"
   log "Restart Apache after installation of mod_php5" do
     level :info
